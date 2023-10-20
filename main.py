abb4c221b3652a904a323f60065a0804126f67cf
Rosya <hailtherosya@gmail.com>
2023-10-20 13:23:53 +0300

    rename name to channel_number

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Channel:
-    name: str
+    chanell_number: str
     freq: int
     diapason: str
 
@@ -13,18 +13,18 @@ class Channel:
 G24 = {6: -87, 9: -86, 12: -85, 18: -83, 24: -80, 36: -76, 48: -71, 54: -66}
 
 G5 = {15: -96, 30: -95, 45: -92, 60: -90, 90: -86, 120: -83, 135: -77, 150: -74}
-SOM = 10
 
+SOM = 10
 P = 10
 Gt = 1
 Gr = 1
 
 
-chanel1 = Channel(name="7", freq=2442, diapason="2.4")
-chanel2 = Channel(name="8", freq=2447, diapason="2.4")
+chanel1 = Channel(chanell_number="7", freq=2442, diapason="2.4")
+chanel2 = Channel(chanell_number="8", freq=2447, diapason="2.4")
 
-chanel3 = Channel(name="100", freq=5500, diapason="5")
-chanel4 = Channel(name="153", freq=5765, diapason="5")
+chanel3 = Channel(chanell_number="100", freq=5500, diapason="5")
+chanel4 = Channel(chanell_number="153", freq=5765, diapason="5")
 
 
 def calc(chan: Channel, frequencies: dict[int, int]) -> list[str | list[int]]:
@@ -48,5 +48,7 @@ plt.plot(
     *calc(chan=chanel3, frequencies=G5),
     *calc(chan=chanel4, frequencies=G5),
 )
+plt.xlabel("Мбит/сек")
+plt.ylabel("Метры")
 plt.grid(True)
 plt.show()
